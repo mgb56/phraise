78345ec54f48f6a7d1f1b7507fd0ed51f605b0af
Michael Linden <m.linden@yale.edu>
2021-01-03 14:26:49 -0700

    preliminary html parsing

--- frontend/foreground.js
+++ frontend/foreground.js
@@ -1,2 +1,24 @@
 // document.body refers to the actual html page
-console.log(document.body);
+//console.log(document.body);
+
+console.log('got here');
+
+
+
+var elements = document.getElementsByTagName('*')
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+        if (node.nodeType === 3) { // if child is a text node
+            var text = node.nodeValue;
+            console.log(text);
+            var replacedText = text.replace('him', 'sdjfldjslfjlskejflijweofgjoewijfoewjfli');
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
