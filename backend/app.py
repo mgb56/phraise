3b91b37bdfa8dd2df9024d1b36e4e3ed46961427
Michael Linden <m.linden@yale.edu>
2021-01-03 19:24:24 -0700

    deployment infra

--- backend/app.py
+++ backend/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', , methods=['POST', 'GET'])
 def hello_world():
-    if request.method == 'POST' and 'sentences' in request.form:
-        json_form = json.loads(request.form)
-        sentences = json_form['sentences']
-        for sentence in sentences:
-            print(sentence)
+    # if request.method == 'POST' and 'sentences' in request.form:
+    #     json_form = json.loads(request.form)
+    #     sentences = json_form['sentences']
+    #     for sentence in sentences:
+    #         print(sentence)
 
-    return ''
+    return 'test to see if this is working'
