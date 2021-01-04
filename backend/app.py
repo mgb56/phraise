fd707cf85e63297dc211229fcfa3a8a8991161ae
Michael Linden <m.linden@yale.edu>
2021-01-03 19:44:04 -0700

    fix syntax issue

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', , methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def hello_world():
     # if request.method == 'POST' and 'sentences' in request.form:
     #     json_form = json.loads(request.form)
