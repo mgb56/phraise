b5e72baad42fd2dea3d5c2d096ce7a96ad903ba8
Michael Linden <m.linden@yale.edu>
2021-01-27 17:43:34 -0700

    fix spacing issue for translations

--- frontend/background.js
+++ frontend/background.js
@@ -9,33 +9,55 @@
 //     console.error(err);
 //   });
 
-var testSentence =
-  "this is a test sentence to see how the spacing works in clauses";
-var testDoc = nlp(testSentence);
-var testClauses = testDoc.clauses().out("array");
-console.log(testClauses);
+// var testSentence =
+//   "this is a test sentence to, see, how  the  spacing   works in clauses";
+// var testDoc = nlp(testSentence);
+// // var testClauses = testDoc.clauses().out("array");
+// var testClauses = testDoc.clauses().out("array");
+// console.log(testDoc.clauses().json());
+// console.log(testDoc.clauses().text());
+// console.log("i wonder if this will work");
+// console.log(testClauses[0].text());
 
 const translate = require("@iamtraction/google-translate");
+
+function convertClauseToString(clause) {
+  var finalString = "";
+  for (var i = 0; i < clause["terms"].length; i++) {
+    finalString +=
+      clause["terms"][i]["pre"] +
+      clause["terms"][i]["text"] +
+      clause["terms"][i]["post"];
+  }
+  return finalString;
+  // return clause["text"];
+}
+
+// console.log(testDoc.clauses().json()[0]);
+// console.log(convertClauseToString(testDoc.clauses().json()[0]));
 
 function transform_clause_into_quartuple(clauses, i) {
   var before_context = "";
   for (var j = 0; j < i; j++) {
-    before_context += clauses[j] + " ";
+    before_context += convertClauseToString(clauses[j]);
+    // before_context += clauses[j]["text"];
   }
-  var phrase_to_be_translated = clauses[j];
+  // var phrase_to_be_translated = clauses[j]["text"];
+  var phrase_to_be_translated = convertClauseToString(clauses[j]);
   var after_context = "";
   for (var j = i + 1; j < clauses.length; j++) {
-    after_context += " " + clauses[j];
+    after_context += convertClauseToString(clauses[j]);
+    // after_context += clauses[j]["text"];
   }
   return [before_context, phrase_to_be_translated, after_context];
 }
 
 function naive_split(sentence, min_length, max_length) {
   var doc = nlp(sentence);
-
-  var clauses = doc.clauses().out("array");
+  var clauses = doc.clauses().json();
+  //var clauses = doc.clauses().out("array");
   for (var i = 0; i < clauses.length; i++) {
-    let clause = clauses[i];
+    let clause = clauses[i]["text"];
     if (min_length <= clause.length && clause.length <= max_length) {
       return transform_clause_into_quartuple(clauses, i);
     }
@@ -47,6 +69,49 @@ function naive_split(sentence, min_length, max_length) {
   }
 }
 
+function isAlphaNumeric(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)
+    ) {
+      // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+}
+
+function count_leading_and_trailing_whitespace(sentence) {
+  let saw_char = false;
+  let num_left_spaces = 0;
+  let num_right_spaces = 0;
+
+  let i = 0;
+  while (i < sentence.length) {
+    if (isAlphaNumeric(sentence[i])) {
+      break;
+    } else {
+      num_left_spaces++;
+    }
+    i++;
+  }
+  i = sentence.length - 1;
+  while (i >= 0) {
+    if (isAlphaNumeric(sentence[i])) {
+      break;
+    } else {
+      num_right_spaces++;
+    }
+    i--;
+  }
+  return [num_left_spaces, num_right_spaces];
+}
+
 async function translate_phrase(
   before_context,
   phrase_to_be_translated,
@@ -54,44 +119,61 @@ async function translate_phrase(
   language
 ) {
   // const delimiter = " ALFSKJLKASF ";
-  const spanOpen = " <span> ";
-  const spanClose = " </span> ";
+  let space_arr = count_leading_and_trailing_whitespace(
+    phrase_to_be_translated
+  );
+  const num_left_spaces = space_arr[0];
+  const num_right_spaces = space_arr[1];
+  phrase_to_be_translated = phrase_to_be_translated.trim();
+  const spanOpen = "<p>";
+  const spanClose = " </p>";
   let full_sentence =
     before_context +
     spanOpen +
     phrase_to_be_translated +
     spanClose +
     after_context;
+  // console.log("about to translate: ");
+  // console.log(full_sentence);
   let translationObj = await translate(full_sentence, {
     from: "en",
     to: language
   });
   let translationString = translationObj["text"];
+  // console.log("the response is: ");
+  // console.log(translationString);
 
-  const left_index = translationString.indexOf("<span>");
-  const right_index = translationString.lastIndexOf("</span>");
+  const left_index = translationString.indexOf("<p>");
+  const right_index = translationString.lastIndexOf("</p>");
 
   var final_left_index = left_index;
   var final_right_index = right_index;
 
-  for (var i = left_index; i < translationString.length; i++) {
-    if (translationString[i] === " ") {
-      final_left_index = i + 1;
-      break;
-    }
-  }
+  // for (var i = left_index; i < translationString.length; i++) {
+  //   if (translationString[i] === " ") {
+  //     final_left_index = i + 1;
+  //     break;
+  //   }
+  // }
 
-  for (var i = right_index; i >= 0; i--) {
-    if (translationString[i] === " ") {
-      final_right_index = i - 1;
-      break;
-    }
-  }
+  // for (var i = right_index; i >= 0; i--) {
+  //   if (translationString[i] === " ") {
+  //     final_right_index = i - 1;
+  //     break;
+  //   }
+  // }
 
-  var result = translationString.slice(final_left_index, final_right_index + 1);
-  if (before_context.length === 0) {
-    result = " " + result;
-  }
+  var result = translationString.slice(
+    final_left_index + 3 + 1,
+    final_right_index
+  );
+  result = " ".repeat(num_left_spaces) + result + " ".repeat(num_right_spaces);
+  // if (before_context.length === 0) {
+  //   result = " " + result;
+  // }
+  // if (after_context.length == 0) {
+  //   result = result + " ";
+  // }
   return [before_context, result, after_context, phrase_to_be_translated];
 }
 
@@ -158,6 +240,15 @@ async function partially_translate_sentences(
 // socket.on("connect", function () {
 //   console.log("Client connected");
 // });
+
+// partially_translate_sentences(
+//   [
+//     "proclaimed its independence with German support and protection. Germany annexed and invaded the Czech "
+//   ],
+//   "es",
+//   10,
+//   50
+// ).then((res) => console.log(res));
 
 chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.set({ color: "#3aa757" }, function () {
